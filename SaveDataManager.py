5a1a386102575cc94dbb9440b7eda6718dd9e51b
Lexa[Heda] <Lexa-Heda@users.noreply.github.com>
2023-08-13 21:17:16 +0200

    clear
    asdsadsd

---
+++ SaveDataManager.py
@@ -0,0 +1,31 @@
+import pickle
+
+
+class SaveDataManager:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def save_data(self, data):
+        with open(self.filename, 'wb') as file:
+            pickle.dump(data, file)
+
+    def load_data(self):
+        try:
+            with open(self.filename, 'rb') as file:
+                data = pickle.load(file)
+                return data
+        except FileNotFoundError:
+            return None
+
+
+# Beispielcode:
+
+leben = 3
+
+file_Leben = SaveDataManager("saves/leben.asduhbvasjlhdvbfashdnfbm")
+
+file_Leben.save_data(leben)
+
+neue_Leben = file_Leben.load_data()
+
+print(str(neue_Leben))
