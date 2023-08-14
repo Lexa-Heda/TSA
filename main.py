a681b2d152e2d6bbf66bdd8481b6febd5eac4085
Lexa[Heda] <Lexa-Heda@users.noreply.github.com>
2023-08-14 13:48:23 +0200

    dt

--- main.py
+++ main.py
@@ -1,34 +1,21 @@
+import pygame, sys
+import time
+import threading
 
 
+class Main():
+    def __init__(self):
+        self.screen = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
 
+    def gameloop(self):
+        while True:
+            for event in pygame.event.get():
+                if event == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    def run(self):
+        self.gameloop()
 
 
 
