098c8a2177dee3d65300f36f3279350348f85bc3
Lexa[Heda] <pokemontrainer1612@gmial.com>
2023-08-17 19:07:59 +0200

    Nico Notebook CODE

--- create_element.py
+++ create_element.py
@@ -12,7 +12,7 @@ class Create_GUI:
     def create_base_menu(self):
         if True:
             image_path = "graphics/png/buttons/start_button.png"
-            self.handler.to_draw.append(Element(image_path, (100, 99)))
+            self.handler.to_draw.append(Element(image_path, (100, 99), scale=8))
             self.btn_handler.buttons.append(Button(image_path, (100, 100), "pygame.quit()"))
         if True:
-            self.handler.to_draw.append(Element("graphics/png/objekts/campfire/", (200, 200), True))
+            self.handler.to_draw.append(Element("graphics/png/objekts/campfire/", (200, 200), True, scale=6))
