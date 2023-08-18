b5fee7e6ffa23c6bb16395e7305d0b803a0a38a1
Sternglas2 <141495818+Sternglas2@users.noreply.github.com>
2023-08-18 16:25:37 +0200

    Code aktualisiert (PC)

--- create_element.py
+++ create_element.py
@@ -14,14 +14,14 @@ class Create_GUI:
         if True:
             image_path = "graphics/png/buttons/start_button.png"
             self.handler.to_draw.append(Element(image_path, (100, 99), scale=8))
-            self.btn_handler.buttons.append(Button(image_path, "self.btn_handler.clear_screen()", self.btn_handler, scale=8))
+            self.btn_handler.buttons.append(Button(image_path, "self.btn_handler.clear_screen()", (100, 99), self.btn_handler, scale=8))
         if True:
             # Stuff um das Lagerfuer mittig zu machen...
             self.handler.to_draw.append(Element("graphics/png/objekts/campfire/", (640, 360), True, scale=6))
         if True:
             image_path = "graphics/png/buttons/exit_button.png"
             self.handler.to_draw.append(Element(image_path, (500, 500), scale=8))
-            self.btn_handler.buttons.append(Button(image_path, "sys.exit()", self.btn_handler, scale=8))
+            self.btn_handler.buttons.append(Button(image_path, "sys.exit()", (100, 99), self.btn_handler, scale=8))
         if True:
             self.handler.to_draw.append(Element("graphics/png/backgrounds/main_menu_background.png", (0, 0), scale=80))
 
