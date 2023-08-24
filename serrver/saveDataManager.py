6d1c0d4959701fabb681d0fe05c1b540ce92f7b1
Lexa[Heda] <135357982+Lexa-Heda@users.noreply.github.com>
2023-08-24 18:41:28 +0200

    Nico PC

--- serrver/saveDataManager.py
+++ serrver/saveDataManager.py
@@ -1,19 +1,16 @@
 import pickle
 
 
-class SaveDataManager:
-    def __init__(self):
-        self.var = 1 + 1
+def save_data(data, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(data, file)
 
-    def save_data(self, data, filename):
-        with open(filename, 'wb') as file:
-            pickle.dump(data, file)
 
-    def load_data(self, filename):
-        try:
-            with open(filename, 'rb') as file:
-                data = pickle.load(file)
-                return data
-        except FileNotFoundError:
-            return None
+def load_data(filename):
+    try:
+        with open(filename, 'rb') as file:
+            data = pickle.load(file)
+            return data
+    except FileNotFoundError:
+        return None
 
--- serrver/server.py
+++ serrver/server.py
@@ -1,15 +1,15 @@
 import socket
 import threading
-from saveDataManager import SaveDataManager
+from saveDataManager import *
 
 clients = []
 
 
 Accounts = {}
 
-SaveDataManager().save_data(Accounts, "serrver/saves/accounts.ss")
+save_data(Accounts, "saves/accounts.ss")
 
-Accounts = SaveDataManager.load_data("serrver/saves/accounts.ss")
+#Accounts = SaveDataManager.load_data("serrver/saves/accounts.ss")
 
 def recive_Data(client):
     data = client[0].recv(1024).decode()
@@ -19,7 +19,7 @@ def recive_Data(client):
 def send_saves(client_number, id):
     for client in clients:
         if client == clients[client_number]:
-            data = save_manager.load_data("serrver/saves/" + str(id) + ".ss")
+            data = save_manager.load_data("saves/" + str(id) + ".ss")
             client[0].send()
 
 
@@ -69,7 +69,7 @@ def main():
                 new_data = {username: len(Accounts)}
                 Accounts.update(new_data)
                 account_id = Accounts[username]
-                save_manager.save_data([], "serrver/saves/" + str(account_id) + ".ss")
+                save_manager.save_data([], "saves/" + str(account_id) + ".ss")
 
             unit = recive_Data(client)
             if unit == "load_data":
@@ -78,7 +78,7 @@ def main():
             elif unit == "save_data":
                 print("send...")
                 data = recive_Data(client)
-                save_manager.save_data(data, "serrver/saves/" + str(account_id) + ".ss")
+                save_manager.save_data(data, "saves/" + str(account_id) + ".ss")
             else:
                 print("Error")
                 print(unit)
