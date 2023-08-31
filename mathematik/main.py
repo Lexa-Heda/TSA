b246c3c2542643c5830c9a7f51b2d32e4739c673
Sternglas2 <141495818+Sternglas2@users.noreply.github.com>
2023-08-31 17:16:41 +0200

    CODE AKTUALISIERT LUDWIG JAKOB SCHEMMERT PC UND PRÄSENtatioN daZu gePAkCackt!111!11!111!!!11!!

--- mathematik/main.py
+++ mathematik/main.py
@@ -1,4 +1,11 @@
-#Autor:      Nico Freitag und Ludwig Schemmert
-#Version:    v0
-#Erstellt:   31.08.2023
-#Bearbeitet: 31.08.2023
+# Autor:      Nico Freitag und Ludwig Schemmert
+# Version:    v0
+# Erstellt:   31.08.2023
+# Bearbeitet: 31.08.2023
+
+def main():
+    pass
+
+
+if __name__ == __main__:
+    main()
