fec25e05f036e7cfd3e02ed86f5089212e2d28e6
Sternglas2 <141495818+Sternglas2@users.noreply.github.com>
2023-08-31 17:54:52 +0200

    CODE AKTUALISIERT LUDWIG JAKOB SCHEMMERT PC UND PRÄSENtatioN daZu gePAkCackt!111!11!111!!!11!!

--- mathematik/main.py
+++ mathematik/main.py
@@ -3,13 +3,34 @@
 # Erstellt:   31.08.2023
 # Bearbeitet: 31.08.2023
 
+from decimal import Decimal, getcontext
+
+
 class Main_class():
     def __init__(self):
-        self.idontknow = True
-    def main_func(self):
-        self.input = input("Gebe eine Zahl ein: ")
-        print(input)
+        getcontext().prec = 16
 
+        # Die Quadratzahl der Wurzel
+        self.radicant = 2
+
+        # Die wievielte Wurzel
+        self.index = 2
+
+        # Die Wurzel
+        self.root_number = Decimal("")
+    def main_funct(self):
+        self.input = input("Gebe eine Zahl ein: ")
+        print(type(self.root_number))
+        print(self.root_number)
+        print()
+        print(len(str(self.root_number)))
+
+
+    def intervall(self):
+        pass
+
+    def heron(self):
+        pass
 
 if __name__ == "__main__":
     main = Main_class()
