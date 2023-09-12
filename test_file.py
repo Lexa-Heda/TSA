f535e0ae6e4141fa803bdc3e56695a0f540906c9
Lexa[Heda] <135357982+Lexa-Heda@users.noreply.github.com>
2023-09-12 19:25:39 +0200

    Java

--- test_file.py
+++ test_file.py
@@ -2,29 +2,9 @@ import tkinter as tk
 import time
 import random
 
-def start_app():
-    # Diese Funktion wird aufgerufen, wenn der Startbutton gedrückt wird.
-    root.destroy()  # Schließe den Startbildschirm
-    # Hier kannst du deine eigentliche App-Logik einfügen
 
-def toggle_label():
-    # Diese Funktion wird verwendet, um den Schriftzug aufflackern zu lassen
-    current_state = label.cget("state")
-    if current_state == "normal":
-        label.config(state="disabled")
-    else:
-        label.config(state="normal")
-    root.after(2000, toggle_label)  # Alle 500 Millisekunden die Funktion erneut aufrufen
+class main:
+    def __int__(self):
+        self.root = tk.Tk()
 
-root = tk.Tk()
-root.title("Start Screen")
 
-label = tk.Label(root, text="Press any button to start", font=("Helvetica", 24))
-label.pack(pady=20)
-
-start_button = tk.Button(root, text="Start", command=start_app)
-start_button.pack()
-
-toggle_label()  # Starte das Aufflackern des Schriftzugs
-
-root.mainloop()
