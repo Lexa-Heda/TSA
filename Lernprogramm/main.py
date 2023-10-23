75deefae7e080aadcbffea44302c8ec69758e063
Lexa-Heda <pokemontrainer1612@gmail.com>
2023-10-23 18:43:28 +0200

    Setup

---
+++ Lernprogramm/main.py
@@ -0,0 +1,17 @@
+import os
+
+
+class App:
+    def __init__(self):
+        self.vocabeln = []
+        self.vocabeln_franz = []
+
+        self.themen_fragen = {
+
+        }
+
+
+
+if __name__ == "__main__":
+    app = App()
+    #app.run()
